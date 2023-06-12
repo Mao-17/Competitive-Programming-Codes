46af34a5e40b3cb0ef4fb637e030a1578c7f1df3
Shivam Gupta <78248576+Mao-17@users.noreply.github.com>
2023-06-12 12:51:41 +0530

    Update Q1.java

--- L1/Q1.java
+++ L1/Q1.java
@@ -1,46 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-
-public class CP1Course {
+ 
+public class Main {
+ 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();  // total number of submissions
-        scanner.nextLine();  // consume the newline character
-        
-        Map<String, Integer> studentPoints = new HashMap<>();
-        
+        int n = scanner.nextInt(); // Length of the array
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            String input = scanner.nextLine();
-            String[] parts = input.split(" ");
-            String studentName = parts[0];
-            int rating = Integer.parseInt(parts[1]);
-            
-            int points = calculatePoints(rating);
-            
-            // Add the points to the existing total for the student
-            studentPoints.put(studentName, studentPoints.getOrDefault(studentName, 0) + points);
+            arr[i] = scanner.nextInt(); // Array elements
         }
-        
-        int maxPoints = 0;
-        
-        // Find the maximum points scored by any student
-        for (int points : studentPoints.values()) {
-            maxPoints = Math.max(maxPoints, points);
+        scanner.close();
+        long result = 0;
+        for(int i = 0; i<n; i++){
+            result+=arr[i]-1;
         }
-        
-        System.out.println(maxPoints);
-    }
-    
-    private static int calculatePoints(int rating) {
-        int baseRating = 1100;
-        int pointsPerInterval = 0;
-        
-        while (baseRating <= rating) {
-            pointsPerInterval++;
-            baseRating += 199;
-        }
-        
-        return pointsPerInterval;  // Adjust for zero-based indexing
+        System.out.println(result);
     }
 }
